9efc895197b1603a52b40f1c5609011cb9e3165c
Zackyrayhan <hargauy@gmail.com>
2025-04-25 19:59:50 +0700

    Add files via upload

---
+++ zacky.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukan kata = PEPEK"<<endl;
+	cin>>kata;
+	
+	if(kata=="PEPEK"){
+		cout<<"Kata yang dimasukan sesuai"<<endl;
+	}else{
+		cout<<"Kata yang dimasukan tidak sesuai"<<endl;
+	}
+	return 0;
+}
